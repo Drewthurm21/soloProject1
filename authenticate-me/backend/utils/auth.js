5cafcf70bd0372487a99b17e62ba37659a3a6582
Drew Thurman <drew@game-rig.localdomain>
2021-03-25 13:38:59 -0700

    added some jwt auth functionality

--- authenticate-me/backend/utils/auth.js
+++ authenticate-me/backend/utils/auth.js
@@ -4,7 +4,6 @@ const { User } = require('../db/models')
 
 const { secret, expiresIn } = jwtConfig
 
-
 // Send a JWT Cookie
 const setTokenCookie = (res, user) => {
 
@@ -15,9 +14,8 @@ const setTokenCookie = (res, user) => {
     { expiresIn: parseInt(expiresIn) },
   );
 
-  const isProduction = process.env.NODE_ENV === "production";
-
   // Set the token cookie
+  const isProduction = process.env.NODE_ENV === "production";
   res.cookie('token', token, {
     maxAge: expiresIn * 1000,
     httpOnly: true,
@@ -27,3 +25,44 @@ const setTokenCookie = (res, user) => {
 
   return token;
 };
+
+
+//Restore authenticated user login
+const restoreUser = (req, res, next) => {
+  // token parsed from cookies
+  const { token } = req.cookies;
+
+  return jwt.verify(token, secret, null, async (err, jwtPayload) => {
+    if (err) {
+      return next();
+    }
+
+    try {
+      const { id } = jwtPayload.data;
+      req.user = await User.scope('currentUser').findByPk(id);
+    } catch (e) {
+      res.clearCookie('token');
+      return next();
+    }
+
+    if (!req.user) res.clearCookie('token');
+
+    return next();
+  });
+};
+
+// If there is no current user, return an error
+const requireAuth = [
+  restoreUser,
+  function (req, res, next) {
+    if (req.user) return next();
+
+    const err = new Error('Unauthorized');
+    err.title = 'Unauthorized';
+    err.errors = ['Unauthorized'];
+    err.status = 401;
+    return next(err);
+  }
+];
+
+module.exports = { setTokenCookie, restoreUser, requireAuth };
