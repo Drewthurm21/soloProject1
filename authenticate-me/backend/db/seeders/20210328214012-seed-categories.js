63b0559638c3ac33481175df77e9766a7acab921
Drewthurm21 <drewthurm21@gmail.com>
2021-03-28 14:50:32 -0700

    adding category table and first seeder

---
+++ authenticate-me/backend/db/seeders/20210328214012-seed-categories.js
@@ -0,0 +1,45 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+    return queryInterface.bulkInsert('People', [
+      { category: 'Business' },
+      { category: 'Travel' },
+      { category: 'Sports' },
+      { category: 'Self Improvement' },
+      { category: 'Food' },
+      { category: 'Music' },
+      { category: 'Philosophy' },
+      { category: 'Religion' },
+      { category: 'Tutorial' },
+      { category: 'Advice' },
+      { category: 'Gaming' },
+      { category: 'Education' },
+      { category: 'History' },
+      { category: 'Ranked Lists' },
+      { category: 'Journal' },
+      { category: 'Photography' },
+      { category: 'Programming' },
+      { category: 'Design' },
+      { category: 'Home Improvement' },
+      { category: 'Gardening' },
+      { category: 'Art' },
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+    return queryInterface.bulkDelete('People', null, {});
+  }
+};
